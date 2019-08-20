0233eed783e8eb9c73507b74d859805f9186e16b
oi <oifuwe@luc.edu>
2019-08-20 16:51:18 -0500

    ...

--- src/main/java/com/trilogyed/taskerconfigserver/TaskerConfigServerApplication.java
+++ src/main/java/com/trilogyed/taskerconfigserver/TaskerConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.trilogyed.taskerconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class TaskerConfigServerApplication {
 
 	public static void main(String[] args) {
