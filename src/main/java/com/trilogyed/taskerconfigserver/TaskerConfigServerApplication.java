c48025f723f2158e6d630c1f97c3cf66130ec92a
oi <oifuwe@luc.edu>
2019-08-20 14:12:54 -0500

    initial commit

---
+++ src/main/java/com/trilogyed/taskerconfigserver/TaskerConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.trilogyed.taskerconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskerConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskerConfigServerApplication.class, args);
+	}
+
+}
